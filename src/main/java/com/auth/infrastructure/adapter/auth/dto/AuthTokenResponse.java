ec57d39cc7dcb60567d43073a97cbb2c75f629cf
Julieta-meza926 <Julietameza926@gmail.com>
2026-06-02 15:23:10 -0300

    feature/TASK-3 add login use case + implement auth0

---
+++ src/main/java/com/auth/infrastructure/adapter/auth/dto/AuthTokenResponse.java
@@ -0,0 +1,16 @@
+package com.auth.infrastructure.adapter.auth.dto;
+
+import lombok.Data;
+
+@Data
+public class AuthTokenResponse {
+
+    private String access_token;
+
+    private String refresh_token;
+
+    private String token_type;
+
+    private Integer expires_in;
+
+}
